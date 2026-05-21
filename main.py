9feec27f29273412870af86844f071c59957fb48
ChocoRamaBG <127840485+ChocoRamaBG@users.noreply.github.com>
2026-05-21 14:59:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -241,8 +241,10 @@ def scrape_framar():
     
     try:
         print("[Инфо] Стартиране на процеса по извличане на данни...")
-        driver.get("https://spravochnik.framar.bg/%D0%BC%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D1%81%D0%BA%D0%B8-%D1%81%D0%BF%D0%B5%D1%86%D0%B8%D0%B0%D0%BB%D0%B8%D1%81%D1%82%D0%B8")
+
         
+        driver.get("https://spravochnik.framar.bg/%D0%B7%D0%B4%D1%80%D0%B0%D0%B2%D0%BD%D0%B8-%D0%B7%D0%B0%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F")
+        #Med specialisti: https://spravochnik.framar.bg/%D0%BC%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D1%81%D0%BA%D0%B8-%D1%81%D0%BF%D0%B5%D1%86%D0%B8%D0%B0%D0%BB%D0%B8%D1%81%D1%82%D0%B8
         decline_cookies(driver)
         
         region_elements = driver.find_elements(By.XPATH, "//a[contains(@href, '-%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82')]")
